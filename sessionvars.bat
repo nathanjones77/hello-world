5bf67b4cafdca79ddf4250e2d1b40ddddc537a67
nathanjones77 <49918622+nathanjones77@users.noreply.github.com>
2019-04-30 13:11:57 +0200

    Create sessionvars.bat

---
+++ sessionvars.bat
@@ -0,0 +1,30 @@
+
+PROJECT_ID=nathan-project-1
+FOLDER=Test
+REGION=europe-west6
+#staging names
+L1=land
+L2=standardised
+L3=conformed
+SRCMGMT=aschema
+SRCTGT=sourcetarget
+
+
+
+echo $FOLDER
+echo $PROJECT_ID
+echo $PROJECT_ID"-raw"
+
+##get the billing information
+#gcloud beta billing accounts list 
+BILLING_ID=01D987-A6DE96-62A98D
+TESTFILE=genius_hip_hop_lyrics.csv
+
+
+echo $FOLDER
+echo $PROJECT_ID
+echo $PROJECT_ID"-raw"
+echo $BILLING_ID
+echo $TESTFILE
+echo $L1
+
